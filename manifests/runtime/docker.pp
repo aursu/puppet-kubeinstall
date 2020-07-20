a8c5a8b9c227d244e25a7042cbc1c59b524fdada
Alexander Ursu <alexander.ursu@gmail.com>
2020-07-20 22:09:33 +0200

    Added kubeinstall::runtime::docker

---
+++ manifests/runtime/docker.pp
@@ -0,0 +1,9 @@
+# @summary Docker container runtime setup
+#
+# Use Docker as container runtime for Kubernetes
+# see https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
+#
+# @example
+#   include kubeinstall::runtime::docker
+class kubeinstall::runtime::docker {
+}
