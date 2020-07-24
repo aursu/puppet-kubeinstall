838e6c90eea4cf78d691d166b9c33c193fd3190f
Alexander Ursu <alexander.ursu@gmail.com>
2020-07-25 00:19:34 +0200

    We do not allow non-numerical kubernetes version

--- types/version.pp
+++ types/version.pp
@@ -1,4 +1,2 @@
-type Kubeinstall::Version = Variant[
-    Enum['present', 'installed', 'absent'],
-    Pattern[/^1\.1[6-8]\./],  # https://kubernetes.io/docs/setup/release/version-skew-policy/
-]
+# https://kubernetes.io/docs/setup/release/version-skew-policy/
+type Kubeinstall::Version = Pattern[/^1\.1[6-8]\.[0-9]+/]
