83170af620bef193b5a0cfda23e335b5de6f0759
Alexander Ursu <alexander.ursu@gmail.com>
2020-08-09 08:30:23 +0200

    Testing join_command resource

--- manifests/kubeadm/join_command.pp
+++ manifests/kubeadm/join_command.pp
@@ -4,8 +4,58 @@
 #
 # @example
 #   include kubeinstall::kubeadm::join_command
-class kubeinstall::kubeadm::join_command {
-  # kubeadm token list -o json
+class kubeinstall::kubeadm::join_command (
+  Optional[Kubeinstall::Token]
+          $token             = undef,
+  Optional[Kubeinstall::CACertHash]
+          $ca_cert_hash      = undef,
+  Optional[Kubeinstall::Address]
+          $apiserver_address = undef,
+  Integer $apiserver_port    = $kubeinstall::apiserver_bind_port,
+  String  $cluster_name      = $kubeinstall::cluster_name,
+  Stdlib::Unixpath
+          $cri_socket        = $kubeinstall::cri_socket,
+  Stdlib::Fqdn
+          $node_name         = $kubeinstall::node_name,
+){
+  # https://godoc.org/k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta2#JoinConfiguration
   # kubeadm config print join-defaults
-  # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#join-nodes
+  $init_header  = {
+    'apiVersion' => 'kubeadm.k8s.io/v1beta2',
+    'kind' => 'JoinConfiguration'
+  }
+
+  $init_base = {
+    'caCertPath' => '/etc/kubernetes/pki/ca.crt',
+    'nodeRegistration' => {
+      'criSocket' => $cri_socket,
+      'name'      => $node_name,
+      'taints'    => nil,
+    }
+  }
+
+  $token_discovery = kubeinstall::discovery_hosts(
+    'Kubeinstall::Token_discovery',
+    ['title', 'ca_cert_hash', 'apiserver_address', 'apiserver_port'],
+    ['cluster_name', '==', $cluster_name],
+  )
+
+  notify { $token_discovery: }
+
+  if $token and $apiserver_address and $ca_cert_hash {
+    $init_discovery = {
+      'discovery' => {
+        'bootstrapToken'    => {
+          'apiServerEndpoint'        => "${apiserver_address}:${apiserver_port}",
+          'token'                    => $token,
+          'unsafeSkipCAVerification' => false,
+          'caCertHashes'             => [
+            $ca_cert_hash,
+          ]
+        },
+        'timeout'           => '5m0s',
+        'tlsBootstrapToken' => $token,
+      }
+    }
+  }
 }
