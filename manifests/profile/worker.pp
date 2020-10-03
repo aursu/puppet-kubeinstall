a79b519805388fdd91107f83df2b8bb9b5003444
Alexander Ursu <alexander.ursu@gmail.com>
2020-10-03 17:14:43 +0200

    Setup cluster role for worker and pass volumes hash into cluster class

--- manifests/profile/worker.pp
+++ manifests/profile/worker.pp
@@ -4,8 +4,23 @@
 #
 # @example
 #   include kubeinstall::profile::worker
-class kubeinstall::profile::worker {
+#
+# @param local_persistent_volumes
+#   Added ability for cluster administrator to setup local persistent volumes
+#   into Kubernetes cluster. This hash should containn only volumes located on
+#   currennt worker node.
+#
+class kubeinstall::profile::worker (
+  Hash[String, Hash]
+          $local_persistent_volumes = {}
+)
+{
   include kubeinstall::profile::kubernetes
+
+  class { 'kubeinstall::cluster':
+    cluster_role             => 'worker',
+    local_persistent_volumes => $local_persistent_volumes,
+  }
 
   class { 'kubeinstall::install::worker': }
 }
