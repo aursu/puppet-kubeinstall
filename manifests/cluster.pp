07af1f5a72d97a9492324c34a8b41ef492fa81b7
Alexander Ursu <alexander.ursu@gmail.com>
2020-10-03 17:12:24 +0200

    Added persistent volumes setup

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -1,16 +1,24 @@
-# @summary A short summary of the purpose of this defined type.
+# @summary Cluster resources definition, export and import
 #
-# A description of what this defined type does
+# Cluster resources definition, export and import
 #
 # @example
 #   include kubeinstall::cluster
+#
+# @param cluster_role
+#   Role inside cluster - either 'worker' or 'controller'
+#
 class kubeinstall::cluster (
   Kubeinstall::Address
-          $apiserver_address = $kubeinstall::apiserver_advertise_address,
-  Integer $apiserver_port    = $kubeinstall::apiserver_bind_port,
-  String  $cluster_name      = $kubeinstall::cluster_name,
+          $apiserver_address        = $kubeinstall::apiserver_advertise_address,
+  Integer $apiserver_port           = $kubeinstall::apiserver_bind_port,
+  String  $cluster_name             = $kubeinstall::cluster_name,
   Optional[Kubeinstall::CACertHash]
-          $ca_cert_hash      = $facts['kubeadm_discovery_token_ca_cert_hash'],
+          $ca_cert_hash             = $facts['kubeadm_discovery_token_ca_cert_hash'],
+  Optional[Enum['controller', 'worker']]
+          $cluster_role             = undef,
+  Hash[Kubeinstall::DNSSubdomain, Kubeinstall::LocalPV]
+          $local_persistent_volumes = {}
 ) {
   if $facts['kubeadm_token_list'] and $facts['kubeadm_token_list'][0] {
     $token_data = $facts['kubeadm_token_list'][0]
@@ -46,7 +54,7 @@ class kubeinstall::cluster (
     ['cluster_name', '==', $cluster_name],
   )
 
-  # if not empty - use first credentials' set
+  # if not empty - use first in set credentials
   if $token_discovery[0] {
     [$join_token, $join_ca_cert_hash, $join_apiserver_address, $join_apiserver_port] = $token_discovery[0]
   }
@@ -55,5 +63,31 @@ class kubeinstall::cluster (
     $join_ca_cert_hash = undef
     $join_apiserver_address =  undef
     $join_apiserver_port = 6443
+  }
+
+  case $cluster_role {
+    'controller': {
+      # Local persistent volumes collect
+      Kubeinstall::Resource::Pv::Local <<| tag == $cluster_name |>>
+    }
+    'worker': {
+      # Local persistent volumes export
+      if $local_persistent_volumes[0] {
+        $local_persistent_volumes.each |$volume, $volume_settings| {
+          # default hostnname is puppet FQDN fact
+          $hostname_param = $volume_settings['hostname'] ? {
+            String  => {},
+            default => { 'hostname' => $facts['fqdn'] },
+          }
+
+          @@kubeinstall::resource::pv::local { $volume:
+            *   => $volume_settings + $hostname_param,
+            tag => $cluster_name,
+          }
+        }
+      }
+    }
+    default: {
+    }
   }
 }
