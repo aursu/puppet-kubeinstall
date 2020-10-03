4b4c38b8d9a76990e0bd5e7a8038078a9609ab5a
Alexander Ursu <alexander.ursu@gmail.com>
2020-10-03 17:11:25 +0200

    Define local persistent volume parameters

---
+++ types/localpv.pp
@@ -0,0 +1,7 @@
+type Kubeinstall::LocalPV = Struct[{
+  path               => Stdlib::Unixpath,
+  volume_storage     => Kubeinstall::Quantity,
+  name               => Optional[Kubeinstall::DNSName],
+  hostname           => Optional[Stdlib::Fqdn],
+  storage_class_name => Optional[String],
+}]
