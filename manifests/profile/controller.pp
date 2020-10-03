8db5bb8b8edce0a9ea21733e78ef711216ce1d6a
Alexander Ursu <alexander.ursu@gmail.com>
2020-10-03 17:13:38 +0200

    Setup cluster role for controller

--- manifests/profile/controller.pp
+++ manifests/profile/controller.pp
@@ -14,6 +14,10 @@ class kubeinstall::profile::controller (
 {
   include kubeinstall::profile::kubernetes
 
+  class { 'kubeinstall::cluster':
+    cluster_role => 'controller',
+  }
+
   class { 'kubeinstall::install::controller': }
   if $helm_client {
     class { 'kubeinstall::install::helm': }
