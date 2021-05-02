91c0bc248c1a5b9028461617215bc3cbb40e537b
Alexander Ursu <alexander.ursu@gmail.com>
2021-05-02 23:49:21 +0200

    Added CRI socket into reset command

--- manifests/kubeadm/reset_command.pp
+++ manifests/kubeadm/reset_command.pp
@@ -6,10 +6,20 @@
 #   include kubeinstall::kubeadm::reset_command
 class kubeinstall::kubeadm::reset_command (
   Integer $verbosity_level = 4,
+  Optional[Stdlib::Unixpath]
+          $cri_socket      = $kubeinstall::cri_socket,
 )
 {
+  # /var/run/crio/crio.sock
+  if $cri_socket {
+    $cri_socket_flag = " --cri-socket=${cri_socket}"
+  }
+  else {
+    $cri_socket_flag = ''
+  }
+
   exec { 'kubeadm-reset':
-    command     => "kubeadm reset -f --v=${verbosity_level}",
+    command     => "kubeadm reset -f --v=${verbosity_level}${cri_socket_flag}",
     path        => '/usr/bin:/bin:/usr/sbin:/sbin',
     onlyif      => [
       'test -f /etc/kubernetes/kubelet.conf',
