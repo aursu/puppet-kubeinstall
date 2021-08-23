7bd5ca088bf4b806ddee8330fe5bc422eae2c1a5
Alexander Ursu <alexander.ursu@gmail.com>
2021-08-23 12:32:58 +0300

    Disable selinux class on Ubuntu systems

--- manifests/system/selinux/noop.pp
+++ manifests/system/selinux/noop.pp
@@ -5,7 +5,10 @@
 # @example
 #   include kubeinstall::system::selinux::noop
 class kubeinstall::system::selinux::noop {
+  # puppet-selinux does not support Ubuntu
+  unless $facts['os']['name'] == 'Ubuntu' {
     class {'selinux':
       mode => 'permissive',
     }
+  }
 }
