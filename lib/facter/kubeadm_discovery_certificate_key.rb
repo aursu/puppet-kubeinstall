90b3c8829e29fefcd108f5174b24c414e1aaa1d1
Alexander Ursu <alexanderu@crytek.de>
2025-04-03 03:27:33 +0200

    Replaced backticks with Puppet default open3

--- lib/facter/kubeadm_discovery_certificate_key.rb
+++ lib/facter/kubeadm_discovery_certificate_key.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'securerandom'
 require 'time'
 require 'fileutils'
+require 'open3'
 
 JSON_FILE = '/etc/puppetlabs/facter/facts.d/kubeadm_certificate_key.json'
 TTL_HOURS = 2
@@ -18,7 +19,7 @@ end
 Facter.add(:kubeadm_discovery_certificate_key) do
   confine do
     File.exist?('/etc/kubernetes/admin.conf') &&
-    File.executable?('/usr/bin/kubeadm')
+      File.executable?('/usr/bin/kubeadm')
   end
 
   setcode do
@@ -33,30 +34,32 @@ Facter.add(:kubeadm_discovery_certificate_key) do
           current_key = key
           Facter.debug("Existing certificate key valid until #{ttl}")
         end
+      rescue JSON::ParserError => e
+        Facter.debug("JSON parsing error in #{JSON_FILE}: #{e.message}")
       rescue => e
-        Facter.debug("Failed to parse #{JSON_FILE}: #{e}")
+        Facter.debug("Unknown error while reading #{JSON_FILE}: #{e.message}")
       end
     end
 
     if current_key.nil?
       new_key = create_certificate_key
       if new_key.nil?
-        Facter.warning("Failed to generate certificate key: #{err}")
         next nil
       end
 
       ttl_time = (Time.now.utc + TTL_HOURS * 3600).iso8601
       cmd = "/usr/bin/kubeadm init phase upload-certs --upload-certs --certificate-key #{new_key}"
-      output = `#{cmd}`
 
-      if $CHILD_STATUS.success?
+      stdout, stderr, status = Open3.capture3(cmd)
+      if status.success?
         current_key = new_key
         FileUtils.mkdir_p(File.dirname(JSON_FILE))
         File.write(JSON_FILE, JSON.pretty_generate({ "key" => current_key, "ttl" => ttl_time }))
+        File.chown(0, 0, JSON_FILE)
         File.chmod(0600, JSON_FILE)
         Facter.debug("Generated new certificate key, valid until #{ttl_time}")
       else
-        Facter.warning("Failed to upload certs: #{output.strip}")
+        Facter.warning("Failed to upload certs: #{stderr.strip}")
         current_key = nil
       end
     end
